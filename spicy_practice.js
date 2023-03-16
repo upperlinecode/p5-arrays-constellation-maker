980e5710a4b2b9b056d8d76ae899d429434f5970
ileini-melendez <122940019+ileini-melendez@users.noreply.github.com>
2023-03-16 17:33:54 -0400

    Create spicy_practice.js

---
+++ spicy_practice.js
@@ -0,0 +1,18 @@
+
+
+// Intro to Code with p5.js: Array (Constellation Maker)
+// Use this file to complete the "Spicy Practice".
+
+
+
+
+function setup(){
+  createCanvas(800, 600);
+  
+}
+
+function draw(){
+  background(220);
+
+  
+}
